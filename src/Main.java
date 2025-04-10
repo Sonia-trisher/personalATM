e1e6a39c373e88ca43a06f96530edb4a18168b4b
sonia-trisher <soniatrisher86@gmail.com>
2025-04-10 15:12:47 +0200

    add PersonalATM

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,8 @@ public class Main {
         System.out.println("Initial balance: " + ATM.checkBalance());
         ATM.deposit(50);
         System.out.println("Deposited: " + 50 + ", New Balance: "+ATM.checkBalance());
-//        System.out.println(ATM.withdraw(30));
         ATM.withdraw(30);
-        System.out.println("Withdrew: " + 30 + ", New Balance: "+ATM.checkBalance());
+        System.out.println("Withdreww: " + 30 + ", New Balance: "+ATM.checkBalance());
 
 //        System.out.println(ATM.checkBalance());
     }
